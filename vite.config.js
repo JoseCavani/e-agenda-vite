13ead5ea17ee7593747349c4c30bdfd49bc2f68c
JoseCavani <ze.carlos98@hotmail.com>
2022-08-18 16:22:24 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { resolve } from "path";
+import {ghPages} from "vite-plugin-gh-pages"; 
 import { defineConfig } from "vite";
+
+
 
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
 
 export default defineConfig({
     base: "/e-agenda-vite/",
+    plugins: [ghPages()],
     root: root,
     build: {
         outDir: outDir,
